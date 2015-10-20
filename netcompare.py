5e5608665fb450cbd7c26ecfb152e75dd50aacd7
Cedric Paillet <c.paillet@criteo.com>
2015-10-20 10:49:58 +0000

    netcompare 0.2 (using geneology_text)

--- netcompare.py
+++ netcompare.py
@@ -18,6 +18,8 @@ from ciscoconfparse import CiscoConfParse
 import sys
 import re
 import os
+import yaml
+import pprint
 
 
 def cli_parser():
@@ -28,18 +30,17 @@ def cli_parser():
                         type=str, help='Origin configuration file')
     parser.add_argument('target', metavar='target',
                         type=str, help='Target configuration file')
-    parser.add_argument('no_command', metavar='no_command',
-                        type=str, help='Negation command keyword')
+    parser.add_argument('vendor', metavar='vendor',
+                        type=str, help='vendor')
     return parser.parse_args()
 
-
-def clean_file(file, no_command):
+def clean_file(file, vendor):
     with open(file) as file_opened:
         list = file_opened.readlines()
 
     list_clean = []
 
-    if no_command == 'undo':
+    if vendor == 'huawei':
         line_parent_sharp = False
         for line in list:
             line_clean = line.rstrip(' \t\r\n\0')
@@ -73,135 +74,83 @@ def clean_file(file, no_command):
                     line_parent_sharp = False
     else:
         for line in list:
-            if 'Current Configuration ...' in line:
-                pass
+            for dont_compare in config[vendor]['dont_compare']:
+                if dont_compare in line:
+                    break
             else:
-                list_clean.append(line.rstrip(' \t\r\n\0'))
-
+                f5_curly_bracket = re.search('^(?P<space>\s*)(?P<begin>.*)\{(?P<inside>.*)\}(?P<end>.*)$', line)
+                if vendor == 'f5' and f5_curly_bracket:
+                    list_clean.append(f5_curly_bracket.group('space')+f5_curly_bracket.group('begin')+"{")
+                    list_clean.append(f5_curly_bracket.group('space')+"  "+f5_curly_bracket.group('inside'))
+                    list_clean.append(f5_curly_bracket.group('space')+"}"+f5_curly_bracket.group('end').rstrip(' \t\r\n\0'))
+                else:
+                    list_clean.append(line.rstrip(' \t\r\n\0'))
     return list_clean
 
-
-def compare_children_prefix_no(obj_origin, obj_target, no_command):
-    command_list = [obj_origin.text]
-    for child in obj_origin.children:
-        match = obj_target.find_objects_w_parents(
-            obj_origin.text,
-            '^'+re.escape(child.text)+'$')
-        if len(match) == 0 and not re.match(
-          "\s*" + no_command + "\s.*", child.text):
-            line_text_without_indent = re.match("(\s*)(.*)", child.text)
-            command_list.append(
-                line_text_without_indent.group(1) +
-                no_command +
-                " " +
-                line_text_without_indent.group(2))
-        elif len(match) == 0 and re.match(
-          "\s*" + no_command + "\s.*", child.text):
-            line_text_without_no = re.match(
-              "(\s*)" + no_command + "\s(.*)", child.text)
-            command_list.append(
-                line_text_without_no.group(1) +
-                line_text_without_no.group(2))
-        elif len(child.children) > 0:
-            for result_recursive in compare_children_prefix_no(
-                    child, obj_target, no_command):
-                command_list.append(result_recursive)
-    if len(command_list) > 1:
-        return command_list
-    else:
-        return []
-
-def compare_children(obj_origin, obj_target):
-    command_list = [obj_origin.text]
-    for child in obj_origin.children:
-        match = obj_target.find_objects_w_parents(
-            obj_origin.text,
-            '^'+re.escape(child.text)+'$')
-        if len(child.children) > 0:
-            for result_recursive in compare_children(child, obj_target):
-                command_list.append(result_recursive)
-        for child_match in match:
-             if child_match.geneology_text == child.geneology_text:
-               break
+def print_line(d,depth=0):
+    for k,v in sorted(d.items(),key=lambda x: x[0]):
+        if isinstance(v, dict):
+            print ("%s" % k)
+            print_line(v,depth+1)
         else:
-             command_list.append(child.text)
-    if len(command_list) > 1:
-        return command_list
-    else:
-        return []
+            print "%s %s" % (k, v)
 
-def merge_commands(list1, list2):
-    command_list = []
-    for line_list1 in list1:
-        if isinstance(line_list1, list) is True:
-            for line_children_list1 in line_list1:
-                command_list.append(line_children_list1)
-            for line_list2 in list2:
-                if isinstance(line_list2, list) is True:
-                    if line_list1[0] == line_list2[0]:
-                        for line_children_list2 in line_list2[1:]:
-                            command_list.append(line_children_list2)
-                        list2.remove(line_list2)
-        else:
-            command_list.append(line_list1)
-    for line_list2 in list2:
-        if isinstance(line_list2, list) is True:
-            for line_children_list2 in line_list2:
-                command_list.append(line_children_list2)
-        else:
-            command_list.append(line_list2)
-    return command_list
-
-
-def netcompare(origin, target, no_command):
-    origin_file = CiscoConfParse(origin, syntax='ios', factory=False)
-    target_file = CiscoConfParse(target, syntax='ios', factory=False)
-    diff_add_no_commands = []
-    for line in origin_file.objs:
-        if line.parent.linenum == line.linenum and line.is_config_line is True:
-            parent_match = target_file.find_objects(
-                '^'+re.escape(line.text)+'$',
-                exactmatch=True, ignore_ws=False)
-            if len(parent_match) == 0 and not re.match(
-              no_command + "\s.*", line.text):
-                diff_add_no_commands.append(no_command + ' ' + line.text)
-            elif len(parent_match) == 0 and re.match(
-              no_command + "\s.*", line.text):
-                line_text_without_no = re.match(
-                  no_command + "\s(.*)", line.text)
-                diff_add_no_commands.append(line_text_without_no.group(1))
-            if len(line.children) > 0 and len(parent_match) != 0:
-                result_comparison = compare_children_prefix_no(
-                    line, target_file, no_command)
-                if len(result_comparison) > 0:
-                    diff_add_no_commands.append(result_comparison)
-    diff_add_commands = []
-    for line in target_file.objs:
-        if line.parent.linenum == line.linenum and line.is_config_line is True:
-            parent_match = origin_file.find_objects(
-                '^'+re.escape(line.text)+'$',
-                exactmatch=True, ignore_ws=False)
-            if len(parent_match) == 0:
-                parent_with_children = target_file.find_all_children(
-                    '^'+re.escape(line.text)+'$')
-                for line_in_parent_with_children in parent_with_children:
-                    diff_add_commands.append(line_in_parent_with_children)
-            if len(line.children) > 0 and len(parent_match) != 0:
-                result_comparison = compare_children(line, origin_file)
-                if len(result_comparison) > 0:
-                    diff_add_commands.append( result_comparison )
-    return merge_commands(diff_add_no_commands, diff_add_commands)
+def netcompare(origin, target, vendor):
+    origin_file = CiscoConfParse(origin, syntax=config[vendor]['CiscoConfParse_syntax'], factory=False)
+    target_file = CiscoConfParse(target, syntax=config[vendor]['CiscoConfParse_syntax'], factory=False)
+    result = {}
+    for line_origin in origin_file.objs:
+        find = 0
+        for line_target in target_file.objs:
+            if line_origin.geneology_text == line_target.geneology_text:
+		find = find + 1
+	if find == 0:
+            pointer = result
+            for cmd in line_origin.geneology_text:
+                if not cmd in pointer:
+                    pointer[cmd] = {}
+                pointer = pointer.get(cmd)
+    for line_target in target_file.objs:
+        find = 0
+        for line_origin in origin_file.objs:
+            if line_origin.geneology_text == line_target.geneology_text:
+		find = find + 1
+	if find == 0:
+            pointer = result
+            size = len(line_target.geneology_text)
+            for cmd in line_target.geneology_text:
+                size = size - 1
+                if re.match("(\s*)(.*)", cmd):
+                	line_text_without_no = re.match("^(\s*)(.*)", cmd)
+                        cmd_no = line_text_without_no.group(1) + config[vendor]['no_command'] + " " + line_text_without_no.group(2)
+                        if cmd_no in pointer:
+                        	break
+                if not cmd in pointer and size == (len(line_target.geneology_text) - 1):
+                        pointer[cmd] = {}
+                if not cmd in pointer:
+                     	if not re.match(config[vendor]['no_command'] + "\s.*", cmd):
+                        	line_text_without_no = re.match("^(\s*)(.*)", cmd)
+                     		cmd = line_text_without_no.group(1) + config[vendor]['no_command'] + " " + line_text_without_no.group(2)
+                     	elif re.match(config[vendor]['no_command'] + "\s.*", cmd):
+                        	line_text_without_no = re.match(config[vendor]['no_command'] + "\s(.*)", cmd)
+                        	cmd = line_text_without_no.group(1)
+                        pointer[cmd] = {}
+                pointer = pointer.get(cmd)
+    return result
 
 
 def main():
     args = cli_parser()
+    global config
+    with open('netcompare.yml', 'r') as f:
+        config = yaml.load(f)
 
-    origin_list = clean_file(args.origin, args.no_command)
-    target_list = clean_file(args.target, args.no_command)
+    origin_list = clean_file(args.origin, args.vendor)
+    target_list = clean_file(args.target, args.vendor)
 
-    display_commands = netcompare(origin_list, target_list, args.no_command)
-    for line in display_commands:
-        print line
+    display_commands = netcompare(origin_list, target_list, args.vendor)
+    print_line(display_commands)
+    
 
 if __name__ == '__main__':
     main()
